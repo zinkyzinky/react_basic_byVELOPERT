24bfa6272e429b75228cfb9eb614c0cd3fead5a4
zinkyzinky <lovefool08@naver.com>
2019-05-12 13:40:15 +0900

    #7 JSX 기본 문법 알아보기

--- src/App.js
+++ src/App.js
@@ -1,48 +1,68 @@
 import React, { Component, Fragment } from 'react';
-
+import './App.css';
+// #7
 class App extends Component {
   render(){
-    const name = "Robert!!";
-    const value = 2;
+    const style = {
+      backgroundColor: 'black',
+      padding: '16px',
+      color: 'white',
+      fontSize: '36px'
+    }
+
     return (
-      <div>
-        hello {name}
-        
-        {/* && 사용해서 true인것만 표현 */}
-        { name === 'velopert!!' && <div>벨로퍼트다!!</div>} 
-
-        { name === 'Robert!!' ? '아이언맨이닷!' : '너는 누구냐!!'}
-
-
-        {/* 조건이 여러개일 경우 */}
-        {
-          (
-            function() {
-            if (value === 1) return <div>아이언맨 ???</div>
-            if (value === 2) return <div>벨로퍼트 ???</div>
-            if (value === 3) return <div>누구 ???</div>
-            return <div>모르겠네...</div>
-            }
-          )()
-        }
-
-        {/* 조건이 여러개일 경우 , 화살표 함수 사용 (바로 실행하는거니까 끝에 () 꼭 붙여주기) 
-            this, arguments, super 개념이 없음!!!
-        */}
-        { 
-          (
-            () => {
-            if (name === 'Robert!!') return <div>아이언맨 ???</div>
-            if (name === 'velopert!!') return <div>벨로퍼트 ???</div>
-            if (name === 'Stave!!') return <div>누구 ???</div>
-            }
-          )()
-        }
-
+      // <div style={style} className="App">
+      <div className="App">
+        안녕하세요~
       </div>
-    );
+    )
   }
 }
+
+// #6 
+// class App extends Component {
+//   render(){
+//     const name = "Robert!!";
+//     const value = 2;
+//     return (
+//       <div>
+//         hello {name}
+        
+//         {/* && 사용해서 true인것만 표현 */}
+//         { name === 'velopert!!' && <div>벨로퍼트다!!</div>} 
+
+//         { name === 'Robert!!' ? '아이언맨이닷!' : '너는 누구냐!!'}
+
+
+//         {/* 조건이 여러개일 경우 */}
+//         {
+//           (
+//             function() {
+//             if (value === 1) return <div>아이언맨 ???</div>
+//             if (value === 2) return <div>벨로퍼트 ???</div>
+//             if (value === 3) return <div>누구 ???</div>
+//             return <div>모르겠네...</div>
+//             }
+//           )()
+//         }
+
+//         {/* 조건이 여러개일 경우 , 화살표 함수 사용 (바로 실행하는거니까 끝에 () 꼭 붙여주기) 
+//             this, arguments, super 개념이 없음!!!
+//         */}
+//         { 
+//           (
+//             () => {
+//             if (name === 'Robert!!') return <div>아이언맨 ???</div>
+//             if (name === 'velopert!!') return <div>벨로퍼트 ???</div>
+//             if (name === 'Stave!!') return <div>누구 ???</div>
+//             }
+//           )()
+//         }
+
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
 
