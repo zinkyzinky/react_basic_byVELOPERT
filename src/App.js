522feaa94fcf1e1610bd3972e7d16fe33b3bb78f
zinkyzinky <lovefool08@naver.com>
2019-05-12 13:13:36 +0900

    JSX 문법 알아보기-1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class App extends Component {
   render(){
     return (
       // 반드시 JSX 문법으로 return 해줘야 함.
-      <div>
-        <h1>안녕하세요 리액트</h1>
-      </div>
+
+      // 묶어줘야 에러가 발생하지 않음.
+      // <div>안녕하세요 리액트</div>
+      // <div>반갑습니다!!!!</div>
+
+      // <div>
+      //   <div>안녕하세요 리액트</div>
+      //   <div>반갑습니다!!!!</div>
+      // </div>
+
+      // 요소 검사에서 보이지 않음 (Fragment)
+      <Fragment>
+        <div>안녕하세요 리액트</div>
+        <div>반갑습니다!!!!</div>
+      </Fragment>
+
     );
   }
 }
